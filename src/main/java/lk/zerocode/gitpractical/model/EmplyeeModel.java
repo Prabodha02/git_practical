675d338c3386ed4c7f726d8573a659692d556c57
Prabodha02 <dinushipathiraja1@gmail.com>
2025-11-24 11:34:48 +0530

    add employee model

---
+++ src/main/java/lk/zerocode/gitpractical/model/EmplyeeModel.java
@@ -0,0 +1,33 @@
+package lk.zerocode.gitpractical.model;
+
+public class EmplyeeModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Double salary;
+    public  String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Double getSalary() {
+        return salary;
+    }
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
