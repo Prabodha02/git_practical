7d07a93566c926401f27cda451d3151cf64e67da
Prabodha02 <dinushipathiraja1@gmail.com>
2025-11-24 10:49:39 +0530

    create a employee

--- src/main/java/lk/zerocode/gitpractical/controller/EmployeeController.java
+++ src/main/java/lk/zerocode/gitpractical/controller/EmployeeController.java
@@ -1,5 +1,15 @@
 package lk.zerocode.gitpractical.controller;
 
+import lk.zerocode.gitpractical.controller.request.EmployeeAddRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+    @PostMapping(value = "/employees")
+    public void addEmployee(@RequestBody EmployeeAddRequest employeeAddRequest){
+        System.out.println("employee added");
+    }
 
 }
